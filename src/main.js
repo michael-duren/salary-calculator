ea2728cfbc6ef8f7a9e88b261c239789595dd29a
Michael Duren <michaeld@michaelduren.com>
2023-04-28 15:20:40 -0500

    changed table to use grid instead of table tags

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 // global variables
 
+// inputs
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const employeeId = document.querySelector('#employee-id');
+const title = document.querySelector('#title');
+const annualSalary = document.querySelector('#annual-salary');
+
+// table
+const table = document.querySelector('#employee-table');
+
+// functions
 const onSubmit = (event) => {
   event.preventDefault();
 };
