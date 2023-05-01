5b039b2079a4d9ef95df490a109cef7325bd14ff
Michael Duren <michaeld@michaelduren.com>
2023-05-01 06:50:36 -0500

    added endpoints for getting single e, adding e, and deleting e, tested all work

--- server/API/Controllers/EmployeesController.cs
+++ server/API/Controllers/EmployeesController.cs
@@ -19,4 +19,41 @@ public class EmployeesController : BaseApiController
     {
         return await _context.Employees.ToListAsync();
     }
+
+    [HttpGet("{id}")] // api/employees/{id}
+    public async Task<ActionResult<Employee>> GetEmployee(Guid id)
+    {
+        var employee = await _context.Employees.FindAsync(id);
+
+        if (employee == null)
+        {
+            return NotFound();
+        }
+
+        return employee;
+    }
+
+    [HttpPost] // api/employees
+    public async Task<ActionResult<Employee>> AddEmployee(Employee employee)
+    {
+        _context.Employees.Add(employee);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetEmployee), new { id = employee.Id }, employee);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteEmployee(Guid id)
+    {
+        var employee = await _context.Employees.FindAsync(id);
+        if (employee == null)
+        {
+            return NotFound();
+        }
+
+        _context.Remove(employee);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
 }
