8085efb387546410063ad880da1e47c099285422
Michael Duren <michaeld@michaelduren.com>
2023-05-03 14:10:48 -0500

    update UI for top level edit menu

--- client-react/src/components/tables/employee-item/EmployeeItem.jsx
+++ client-react/src/components/tables/employee-item/EmployeeItem.jsx
@@ -5,11 +5,13 @@ import {
 } from '../../../utils/currency';
 import agent from '../../../api/agent';
 import Spinner from '../../spinner/Spinner';
+import { HiPencilAlt } from 'react-icons/hi';
 
 export default function EmployeeItem(props) {
   const { employee, loading, setLoading, setEmployees, onUpdateEmployee } =
     props;
   const [target, setTarget] = useState('');
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isEditMenuOpen, setIsEditMenuOpen] = useState(false);
   const [editEmployee, setEditEmployee] = useState(employee);
   const { id, employeeId, firstName, lastName, title, salary } = editEmployee;
@@ -72,78 +74,112 @@ export default function EmployeeItem(props) {
         </>
       ) : (
         <>
-          <div className="col-span-2">{employee.firstName}</div>
-          <div className="col-span-2">{employee.lastName}</div>
-          <div className="col-span-2">{employee.employeeId}</div>
-          <div className="col-span-2">{employee.title}</div>
-          <div className="col-span-2">{convertToCurrency(employee.salary)}</div>
+          <div className="col-span-2 p-1">{employee.firstName}</div>
+          <div className="col-span-2 p-1">{employee.lastName}</div>
+          <div className="col-span-2 p-1">{employee.employeeId}</div>
+          <div className="col-span-2 p-1">{employee.title}</div>
+          <div className="col-span-2 p-1">
+            {convertToCurrency(employee.salary)}
+          </div>
         </>
       )}
       {/* Buttons */}
-      {!isEditMenuOpen ? (
-        <div className="col-span-2 flex items-start justify-start">
-          <button
-            className="cursor-pointer"
-            onClick={() => setIsEditMenuOpen(!isEditMenuOpen)}
-            name={id}
-          >
-            {' '}
-            {loading && target === id ? (
-              <Spinner size={15} />
-            ) : (
-              <div className="text-base text-blue-500 hover:text-blue-700">
-                Edit
-              </div>
-            )}
-          </button>
-          <button
-            onClick={(e) => onRemoveEmployee(e, id)}
-            className="cursor-pointer"
-            name={id}
-          >
-            {' '}
-            {loading && target === id ? (
-              <Spinner size={15} />
-            ) : (
-              <div className="ml-6 text-base text-red-500 hover:text-red-700">
-                Delete
-              </div>
-            )}
-          </button>
-        </div>
+      {!isMenuOpen ? (
+        <button
+          onClick={() => setIsMenuOpen(!isEditMenuOpen)}
+          className="flex items-center justify-center"
+        >
+          <HiPencilAlt
+            className="text-gray-500 hover:text-gray-700"
+            size={24}
+          />
+        </button>
       ) : (
-        <div className="col-span-2 flex items-start justify-start">
-          <button
-            className="cursor-pointer"
-            onClick={() =>
-              onUpdateEmployee(editEmployee, isEditMenuOpen, setIsEditMenuOpen)
-            }
-            name={id}
-          >
-            {' '}
-            {loading && target === id ? (
-              <Spinner size={15} />
-            ) : (
-              <div className="text-base text-teal-500 hover:text-teal-700">
-                Update
-              </div>
-            )}
-          </button>
-          <button
-            onClick={() => setIsEditMenuOpen(!isEditMenuOpen)}
-            className="cursor-pointer"
-            name={id}
-          >
-            {' '}
-            {loading && target === id ? (
-              <Spinner size={15} />
-            ) : (
-              <div className="ml-6 text-base text-blue-500 hover:text-blue-700">
-                Back
-              </div>
-            )}
-          </button>
-        </div>
+        <>
+          {!isEditMenuOpen ? (
+            <div className="col-span-2 flex items-start justify-start">
+              <button
+                className="cursor-pointer"
+                onClick={() => setIsEditMenuOpen(!isEditMenuOpen)}
+                name={id}
+              >
+                {' '}
+                {loading && target === id ? (
+                  <Spinner size={15} />
+                ) : (
+                  <div className="text-base text-blue-500 hover:text-blue-700">
+                    Edit
+                  </div>
+                )}
+              </button>
+              <button
+                onClick={(e) => onRemoveEmployee(e, id)}
+                className="cursor-pointer"
+                name={id}
+              >
+                {' '}
+                {loading && target === id ? (
+                  <Spinner size={15} />
+                ) : (
+                  <div className="ml-6 text-base text-red-500 hover:text-red-700">
+                    Delete
+                  </div>
+                )}
+              </button>
+              <button
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className="cursor-pointer"
+                name={id}
+              >
+                {' '}
+                {loading && target === id ? (
+                  <Spinner size={15} />
+                ) : (
+                  <div className="ml-6 text-base text-gray-500 hover:text-gray-700">
+                    X
+                  </div>
+                )}
+              </button>
+            </div>
+          ) : (
+            <div className="col-span-2 flex items-start justify-start">
+              <button
+                className="cursor-pointer"
+                onClick={() =>
+                  onUpdateEmployee(
+                    editEmployee,
+                    isEditMenuOpen,
+                    setIsEditMenuOpen
+                  )
+                }
+                name={id}
+              >
+                {' '}
+                {loading && target === id ? (
+                  <Spinner size={15} />
+                ) : (
+                  <div className="text-base text-teal-500 hover:text-teal-700">
+                    Update
+                  </div>
+                )}
+              </button>
+              <button
+                onClick={() => setIsEditMenuOpen(!isEditMenuOpen)}
+                className="cursor-pointer"
+                name={id}
+              >
+                {' '}
+                {loading && target === id ? (
+                  <Spinner size={15} />
+                ) : (
+                  <div className="ml-6 text-base text-blue-500 hover:text-blue-700">
+                    Back
+                  </div>
+                )}
+              </button>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
