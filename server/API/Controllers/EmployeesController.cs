ba576d3612d2ef0d66af75a0d54945141ae7d74b
Michael Duren <michaeld@michaelduren.com>
2023-05-03 13:39:21 -0500

    wrote put api endpoint and logic

--- server/API/Controllers/EmployeesController.cs
+++ server/API/Controllers/EmployeesController.cs
@@ -15,13 +15,13 @@ public class EmployeesController : BaseApiController
     }
 
     [HttpGet] // api/employees
-    public async Task<ActionResult<List<Employee>>> GetEmployees()
+    public async Task<ActionResult<List<EmployeeDTO>>> GetEmployees()
     {
-        return await _context.Employees.ToListAsync();
+        return await _context.Employees.Select(e => EmployeeToDTO(e)).ToListAsync();
     }
 
     [HttpGet("{id}")] // api/employees/{id}
-    public async Task<ActionResult<Employee>> GetEmployee(Guid id)
+    public async Task<ActionResult<EmployeeDTO>> GetEmployee(Guid id)
     {
         var employee = await _context.Employees.FindAsync(id);
 
@@ -30,7 +30,7 @@ public class EmployeesController : BaseApiController
             return NotFound();
         }
 
-        return employee;
+        return EmployeeToDTO(employee);
     }
 
     [HttpPost] // api/employees
@@ -40,6 +40,42 @@ public class EmployeesController : BaseApiController
         await _context.SaveChangesAsync();
 
         return CreatedAtAction(nameof(GetEmployee), new { id = employee.Id }, employee);
+    }
+
+    [HttpPut] // api/employees
+    public async Task<ActionResult<Employee>> UpdateEmployee(Guid id, EmployeeDTO employeeDTO)
+    {
+        if (id != employeeDTO.Id)
+        {
+            return BadRequest();
+        }
+
+        var employee = await _context.Employees.FindAsync(id);
+
+        if (employee == null)
+        {
+            return NotFound();
+        }
+
+        employee.FirstName = employeeDTO.FirstName;
+        employee.LastName = employeeDTO.LastName;
+        employee.EmployeeId = employeeDTO.EmployeeId;
+        employee.Title = employeeDTO.Title;
+        employee.Salary = employeeDTO.Salary;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!EmployeeExists(id))
+            {
+                return NotFound();
+            }
+        }
+
+        return NoContent();
     }
 
     [HttpDelete("{id}")]
@@ -56,4 +92,20 @@ public class EmployeesController : BaseApiController
 
         return NoContent();
     }
+
+    private bool EmployeeExists(Guid id)
+    {
+        return _context.Employees.Any(e => e.Id == id);
+    }
+
+    private static EmployeeDTO EmployeeToDTO(Employee employee) =>
+        new EmployeeDTO
+        {
+            Id = employee.Id,
+            FirstName = employee.FirstName,
+            LastName = employee.LastName,
+            EmployeeId = employee.EmployeeId,
+            Title = employee.Title,
+            Salary = employee.Salary
+        };
 }
--- server/API/Model/Employee.cs
+++ server/API/Model/Employee.cs
@@ -8,5 +8,18 @@ namespace API.Model
         public string EmployeeId { get; set; }
         public string Title { get; set; }
         public ulong Salary { get; set; }
+        public string Secret { get; set; }
+    }
+
+    // DDTO - Data Transfer Object, hide secret propeties if necessary
+    // currently no use for this, but if need be it's here
+    public class EmployeeDTO
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmployeeId { get; set; }
+        public string Title { get; set; }
+        public ulong Salary { get; set; }
     }
 }
